c4faae8ef810627e9c83781bfeffc3dda844071f
iwhalen <ianpatrickwhalen@gmail.com>
2026-02-04 17:30:22 -0600

    Initial commit (forked from ydataai/ydata-profiling)

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from distutils.core import setup
+from pathlib import Path
+
+from setuptools import setup
+
+# Read the contents of README file
+source_root = Path(".")
+
+try:
+    version = (source_root / "VERSION").read_text().rstrip("\n")
+except FileNotFoundError:
+    version = "0.0.dev0"
+
+with open(source_root / "src/ydata_profiling/version.py", "w") as version_file:
+    version_file.write(f"__version__ = '{version}'")
 
 setup(
-    name='pandas-profiling',
-    version='0.1.0',
-    author='Jos Polfliet',
-    author_email='jos.polfliet+panpro@gmail.com',
-    packages=['pandas-profiling'],
-    url='http://github.com/jospolfliet/pandas-profiling',
-    license='LICENSE',
-    description='Generate profile report for pandas DataFrame',
-    long_description=open('README.md').read(),
-    install_requires=[
-        "pandas",
-        "matplotlib"
-    ],
+    version=version,
 )
